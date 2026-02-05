09c3f99b49988661a66355fd5b18e64c513b9971
tbrockman <iam@theo.lol>
2026-02-05 09:16:33 -0800

    Update template.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,9 @@
-use crate::{exports::witmproxy::plugin::witm_plugin::{CapabilityProvider, Guest, HandleRequestResult, HandleResponseResult, PluginManifest, Request, Response}, witmproxy::plugin::capabilities::{Capabilities, ConnectCapability, RequestCapability, ResponseCapability}};
+use crate::{
+    exports::witmproxy::plugin::witm_plugin::{
+        Capability, CapabilityProvider, Guest, PluginManifest,
+    },
+    witmproxy::plugin::capabilities::{CapabilityKind, CapabilityScope, Event, EventKind},
+};
 
 wit_bindgen::generate!({
     world: "witmproxy:plugin/plugin",
@@ -18,29 +23,34 @@ impl Guest for Plugin {
             version: "0.0.0".to_string(),
             description: "{{description}}".to_string(),
             metadata: vec![],
-            capabilities: Capabilities {
-                connect: ConnectCapability {
-                    filter: "true".to_string(),
+            capabilities: vec![
+                Capability {
+                    kind: CapabilityKind::HandleEvent(EventKind::Connect),
+                    scope: CapabilityScope {
+                        expression: "true".into(),
+                    },
                 },
-                request: Some(RequestCapability {
-                    filter: "true".to_string(),
-                }),
-                response: Some(ResponseCapability {
-                    filter: "true".to_string(),
-                }),
-            },
+                Capability {
+                    kind: CapabilityKind::HandleEvent(EventKind::Request),
+                    scope: CapabilityScope {
+                        expression: "true".into(),
+                    },
+                },
+                Capability {
+                    kind: CapabilityKind::HandleEvent(EventKind::Response),
+                    scope: CapabilityScope {
+                        expression: "true".into(),
+                    },
+                },
+            ],
             license: "MIT".to_string(),
             url: "https://example.com".to_string(),
             publickey: PUBLIC_KEY_BYTES.to_vec(),
         }
     }
 
-    fn handle_request(req: Request, cap: CapabilityProvider) -> HandleRequestResult {
-        HandleRequestResult::Next(req)
-    }
-
-    fn handle_response(res: Response, cap: CapabilityProvider) -> HandleResponseResult {
-        HandleResponseResult::Next(res)
+    fn handle(ev: Event, _cp: CapabilityProvider) -> Option<Event> {
+        Some(ev)
     }
 }
 
